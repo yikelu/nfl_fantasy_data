9fa382e1ee6376d398bad737eca91c9972bfd32e
Yike Lu <yikelu.home@gmail.com>
2014-08-21 16:11:24 -0500

    Header cleaning; auto pickle

--- htmls2csvs.py
+++ htmls2csvs.py
@@ -30,27 +30,57 @@ def html2df(soup):
 
     return df
 
-def position_html_local(posn_name):
+def position_html_local(posn):
     dflist = []
     for ii in range(1, 17):
-        fname = '%s%s.html' % (posn_name, ii)
+        fname = '%s%s.html' % (posn, ii)
         with open(fname) as f:
             df = html2df(BeautifulSoup(f))
         df['wk'] = ii
+        df.columns = header_clean(df.columns, posn)
         dflist.append(df)
     return pd.concat(dflist)
 
-def position_html(posn_name):
+def position_html(posn):
     dflist = []
     for ii in range(1, 17):
-        fname = make_url(posn_name, ii)
-        with urlopen(fname) as f:
-            df = html2df(BeautifulSoup(f))
+        fname = make_url(posn, ii)
+        df = html2df(BeautifulSoup(urlopen(fname)))
         df['wk'] = ii
+        df.columns = header_clean(df.columns, posn)
         dflist.append(df)
     return pd.concat(dflist)
+
+pos_header_suffixes = {
+    'qb': ['_pass', '_rush'],
+    'rb': ['_rush', '_recv'],
+    'wr': ['_recv'],
+    'te': ['_recv'],
+}
+
+exclude_cols = ['rk', 'player', 'team', 'pos', 'fantasy points',
+'wk', 'fum', 'lost', 'qb rating']
+
+def header_clean(header, posn):
+    res = []
+    if posn in pos_header_suffixes:
+        suffixes = pos_header_suffixes[posn]
+        seen_dict = {hh: 0 for hh in header}
+        for hh in header:
+            if not hh in exclude_cols:
+                hres = hh + suffixes[seen_dict[hh]]
+                seen_dict[hh] += 1
+                res.append(hres)
+            else:
+                res.append(hh)
+    else:
+        res = header
+
+    return res
+
 
 if __name__ == '__main__':
     data_all = {}
-    for posn in ['qb', 'wr', 'rb', 'te']:
-        data_all[posn] = position_html(posn)
+    for pp in ['qb', 'wr', 'rb', 'te']:
+        data_all[pp] = position_html_local(pp)
+        data_all[pp].to_pickle('%s.pkl' % pp)
